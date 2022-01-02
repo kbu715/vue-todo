c29e803363749d5c41e58397cd00828deddacb65
kbu715 <kbu715@gmail.com>
2022-01-02 16:40:29 +0900

    feat: add font-awesome, ubuntu, and styles

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-    header
-  </div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 <script>
 export default {
   // js script
 }
 </script>
-<style>
+<style scoped>
   /* css */
+  h1 {
+    color: #2f3b52;
+    font-weight: 900;
+    margin: 2.5rem 1.5rem;
+    font-family: 'Ubuntu', sans-serif;
+  }
 </style>
