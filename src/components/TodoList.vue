f31a916a330c59ff00b8c211699cc9ace0bdc86f
kbu715 <kbu715@gmail.com>
2022-01-03 21:05:34 +0900

    refactor: using props

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+      <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
         <div>
           <i class="checkBtn fas fa-check" 
             v-bind:class="{checkBtnCompleted: todoItem.completed}" 
@@ -17,11 +17,7 @@
 </template>
 <script>
 export default {
-  data: function() {
-    return {
-      todoItems: []
-    }
-  },
+  props: ['propsdata'],
   methods: {
     removeTodo: function(todoItem, index) {
       localStorage.removeItem(todoItem);
@@ -34,15 +30,6 @@ export default {
       localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     }
   },
-  created: function() {
-    if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-        }
-      }
-    }
-  }
 }
 </script>
 <style scoped>
