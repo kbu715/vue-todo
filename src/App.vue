25a2e1b49e883ed45c3ea676d762719c7b038aab
kbu715 <kbu715@gmail.com>
2022-01-06 22:24:23 +0900

    refactor: 할 일 삭제

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <!-- v-on: 하위 컴포넌트에서 발생시킨 이벤트 이름 = "현재 컴포넌트의 메서드 명" -->
     <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
     <!-- v-bind:내려보낼 프롭스 속성 이름 = "현재 위치의 컴포넌트 데이터 속성" -->
-    <TodoList v-bind:propsdata="todoItems"></TodoList>
+    <TodoList v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem"></TodoList>
     <TodoFooter></TodoFooter>
   </div>
 </template>
@@ -30,6 +30,10 @@ export default {
             }
             localStorage.setItem(todoItem, JSON.stringify(obj));
             this.todoItems.push(obj);
+    },
+    removeOneItem: function(todoItem, index) {
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index, 1);
     }
   },
   created: function() {
