83497cf334dc7c629d3ad6fe98c6b1b219fccbff
kbu715 <kbu715@gmail.com>
2022-01-09 15:22:31 +0900

    style: 리스트 아이템 트랜지션

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ul>
+    <transition-group name="list" tag="ul">
       <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
         <div>
           <i class="checkBtn fas fa-check" 
@@ -12,7 +12,7 @@
           <i class="fas fa-trash-alt"></i>
         </span>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 <script>
@@ -65,5 +65,14 @@ export default {
 
   .removeBtn {
     color: #de4343;
+  }
+
+   /* 리스트 아이템 트랜지션 효과 */
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+  .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+    opacity: 0;
+    transform: translateY(30px);
   }
 </style>
