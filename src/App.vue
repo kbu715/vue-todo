5fed6b66853a3c39ba8aa34bb5aea2a528a6f512
kbu715 <kbu715@gmail.com>
2022-01-15 19:42:56 +0900

    delete: unnecessary codes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <!-- v-on: 하위 컴포넌트에서 발생시킨 이벤트 이름 = "현재 컴포넌트의 메서드 명" -->
     <TodoInput></TodoInput>
-    <!-- v-bind:내려보낼 프롭스 속성 이름 = "현재 위치의 컴포넌트 데이터 속성" -->
     <TodoList></TodoList>
     <TodoFooter></TodoFooter>
   </div>
